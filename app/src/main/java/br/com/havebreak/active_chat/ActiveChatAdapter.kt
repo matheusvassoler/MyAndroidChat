7695c91e5ea0fb5b9537367bb5887ebad3e3330f
Matheus da Silva Vassoler <mvassoler@ciandt.com>
2020-03-31 09:04:03 -0300

    Change ActiveChatAdapter

--- app/src/main/java/br/com/havebreak/active_chat/ActiveChatAdapter.kt
+++ app/src/main/java/br/com/havebreak/active_chat/ActiveChatAdapter.kt
@@ -9,24 +9,36 @@ import android.widget.TextView
 import br.com.havebreak.R
 import br.com.havebreak.model.Contact
 import br.com.havebreak.model.Message
+import java.util.logging.Logger
 
 class ActiveChatAdapter(var loggedContact: Contact, var contactToChat: Contact, var messageList: List<Message>) : BaseAdapter() {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var messageInflater:LayoutInflater = parent!!.context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        //var messageInflater:LayoutInflater = parent!!.context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         var message: Message = messageList.get(position)
-        var convertView = convertView
-
+        //var convertView = convertView
+        var view:View
         if(message.sender.id == loggedContact.id) {
-            convertView = messageInflater.inflate(R.layout.my_message, null)
-            var txtMessage:TextView = convertView.findViewById(R.id.my_message_message)
+            view = LayoutInflater.from(parent!!.context).inflate(R.layout.my_message, parent, false)
+            var txtMessage:TextView = view.findViewById(R.id.my_message_message)
             txtMessage.text = message.text
         } else {
-            convertView = messageInflater.inflate(R.layout.their_message, null)
-            var txtMessage:TextView = convertView.findViewById(R.id.their_message_message)
+            view = LayoutInflater.from(parent!!.context).inflate(R.layout.their_message, parent, false)
+            var txtMessage:TextView = view.findViewById(R.id.their_message_message)
             txtMessage.text = message.text
         }
 
-        return convertView
+        /*
+        if(message.sender.id == loggedContact.id) {
+            convertView = messageInflater.inflate(R.layout.my_message, parent, false)
+            var txtMessage:TextView = convertView.findViewById(R.id.my_message_message)
+            txtMessage.text = message.text
+        } else {
+            convertView = messageInflater.inflate(R.layout.their_message, parent, false)
+            var txtMessage:TextView = convertView.findViewById(R.id.their_message_message)
+            txtMessage.text = message.text
+        }*/
+
+        return view
     }
 
     override fun getItem(position: Int): Any {
